87eacfdf4168cfd148d31e7bc867da96261f15b3
doorbell07 <gd268@cornell.edu>
2020-10-03 12:59:14 -0400

    its works mostly now
    still has issues with fitted peaks and uses mysterious concavity for that weird inflection point

--- Code/Data_Analysis_MATLAB/plotAnalysis.m
+++ Code/Data_Analysis_MATLAB/plotAnalysis.m
@@ -58,67 +58,67 @@ for i=2:size(fftMat,1)       %loop through environment
             
             
 
-% %graph fitted peaks and plot fitted peak points
-            if typeOfData==1
-                index1=find(fftMat{i,j}(:,1) == rawPeaksMat{i,j}(1,1));
-                index1Min=index1-10;
-                index1Max=index1+10;
-                fitX1=f(index1Min:index1Max);
-                fitCurve1=polyfit(fitX1,smoothedData(index1Min:index1Max),2);
-                fitY1=polyval(fitCurve1,fitX1);
-                plot(fitX1,fitY1,'Color',[0.4660 0.6740 0.1880],'Linewidth',1.5);
-                [maxVal1,I1]=max(fitY1);
-                if size(rawPeaksMat{i,j},1)>1
-                    index2=find(fftMat{i,j}(:,1) == rawPeaksMat{i,j}(2,1));
-                    index2Min=index2-10;
-                    index2Max=index2+10;
-                    fitX2=f(index2Min:index2Max)';
-                    fitCurve2=polyfit(fitX2,smoothedData(index2Min:index2Max),2);
-                    fitY2=polyval(fitCurve2,fitX2);
-                    plot(fitX2,fitY2,'Color',[0.4660 0.6740 0.1880],'Linewidth',1.5);
-                    [maxVal2,I2]=max(fitY2);
-                    fitPeaksMat(i,j)={[[fitX1(I1); fitX2(I2)],[fitY1(I1); fitY2(I2)]]};
-                    plot(fitPeaksMat{i,j}(1,1),fitPeaksMat{i,j}(1,2),'k*','HandleVisibility','off');
-                    plot(fitPeaksMat{i,j}(2,1),fitPeaksMat{i,j}(2,2),'k*','HandleVisibility','off');
-                else
-                    fitPeaksMat(i,j)={[fitX1(I1),fitY1(I1)]};
-                    plot(fitPeaksMat{i,j}(1,1),fitPeaksMat{i,j}(1,2),'k*','HandleVisibility','off');
-                end
-                integratedPower=trapz(smoothedData);
-                integPowerMat(i,j)={integratedPower};
-            else
-                fitPeaksArray=[];
-                pointsOnEachSide=10;    %number of points to take on each side of the raw peak
-                for l=1:size(rawPeaksMat{i,j},1)
-                    index1=find(fftMat{i,j}(:,1) == rawPeaksMat{i,j}(l,1)); %find index of the raw peak in fftMat
-                    if index1-pointsOnEachSide<2    %check that index1Min is within range of points
-                        index1Min=2;    %not in range so make it the first value
-                    else
-                        index1Min=index1-pointsOnEachSide;  %in range
-                    end
-                    if index1+pointsOnEachSide>size(smoothedData)   %check that index1Min is within range of points
-                        index1Max=size(smoothedData);   %not in range so make it the last value
-                    else
-                        index1Max=index1+pointsOnEachSide;  %in range
-                    end
-                    fitX1=f(index1Min:index1Max);
-                    fitCurve1=polyfit(fitX1,smoothedData(index1Min:index1Max),2);
-                    fitY1=polyval(fitCurve1,fitX1);
-                    plot(fitX1,fitY1,'Color',[0.4660 0.6740 0.1880],'Linewidth',1.5);
-                    %[maxVal1,I1]=max(fitY1);
-                    concavity=diff(fitY1,2);
-                    [B,I]=sort(concavity);
-                    fitPeaksArray=[fitPeaksArray; fitX1(I(1)),fitY1(I(1))];
-                    %fitPeaksArray=[fitPeaksArray; fitX1(I1),fitY1(I1)];
-                    plot(fitPeaksArray(l,1),fitPeaksArray(l,2),'k*','HandleVisibility','off');
-                end
-                fitPeaksMat(i,j)={fitPeaksArray};
-            end
-            %P,ftunnel,windspeed,diameter,sheddingfreq,apeak,fpeak
-            graphData=[graphData;[repmat(pval,size(fitPeaksMat{i,j},1),1),...
-            repmat(fTunnel,size(fitPeaksMat{i,j},1),1),repmat(windSpeed,size(fitPeaksMat{i,j},1),1),...
-            repmat(diameter,size(fitPeaksMat{i,j},1),1),repmat(sheddingFreq,size(fitPeaksMat{i,j},1),1),...
-            fitPeaksMat{i,j}(:,2),fitPeaksMat{i,j}(:,1)]];
+% % %graph fitted peaks and plot fitted peak points
+%             if typeOfData==1
+%                 index1=find(fftMat{i,j}(:,1) == rawPeaksMat{i,j}(1,1));
+%                 index1Min=index1-10;
+%                 index1Max=index1+10;
+%                 fitX1=f(index1Min:index1Max);
+%                 fitCurve1=polyfit(fitX1,smoothedData(index1Min:index1Max),2);
+%                 fitY1=polyval(fitCurve1,fitX1);
+%                 plot(fitX1,fitY1,'Color',[0.4660 0.6740 0.1880],'Linewidth',1.5);
+%                 [maxVal1,I1]=max(fitY1);
+%                 if size(rawPeaksMat{i,j},1)>1
+%                     index2=find(fftMat{i,j}(:,1) == rawPeaksMat{i,j}(2,1));
+%                     index2Min=index2-10;
+%                     index2Max=index2+10;
+%                     fitX2=f(index2Min:index2Max)';
+%                     fitCurve2=polyfit(fitX2,smoothedData(index2Min:index2Max),2);
+%                     fitY2=polyval(fitCurve2,fitX2);
+%                     plot(fitX2,fitY2,'Color',[0.4660 0.6740 0.1880],'Linewidth',1.5);
+%                     [maxVal2,I2]=max(fitY2);
+%                     fitPeaksMat(i,j)={[[fitX1(I1); fitX2(I2)],[fitY1(I1); fitY2(I2)]]};
+%                     plot(fitPeaksMat{i,j}(1,1),fitPeaksMat{i,j}(1,2),'k*','HandleVisibility','off');
+%                     plot(fitPeaksMat{i,j}(2,1),fitPeaksMat{i,j}(2,2),'k*','HandleVisibility','off');
+%                 else
+%                     fitPeaksMat(i,j)={[fitX1(I1),fitY1(I1)]};
+%                     plot(fitPeaksMat{i,j}(1,1),fitPeaksMat{i,j}(1,2),'k*','HandleVisibility','off');
+%                 end
+%                 integratedPower=trapz(smoothedData);
+%                 integPowerMat(i,j)={integratedPower};
+%             else
+%                 fitPeaksArray=[];
+%                 pointsOnEachSide=10;    %number of points to take on each side of the raw peak
+%                 for l=1:size(rawPeaksMat{i,j},1)
+%                     index1=find(fftMat{i,j}(:,1) == rawPeaksMat{i,j}(l,1)); %find index of the raw peak in fftMat
+%                     if index1-pointsOnEachSide<2    %check that index1Min is within range of points
+%                         index1Min=2;    %not in range so make it the first value
+%                     else
+%                         index1Min=index1-pointsOnEachSide;  %in range
+%                     end
+%                     if index1+pointsOnEachSide>size(smoothedData)   %check that index1Min is within range of points
+%                         index1Max=size(smoothedData);   %not in range so make it the last value
+%                     else
+%                         index1Max=index1+pointsOnEachSide;  %in range
+%                     end
+%                     fitX1=f(index1Min:index1Max);
+%                     fitCurve1=polyfit(fitX1,smoothedData(index1Min:index1Max),2);
+%                     fitY1=polyval(fitCurve1,fitX1);
+%                     plot(fitX1,fitY1,'Color',[0.4660 0.6740 0.1880],'Linewidth',1.5);
+%                     %[maxVal1,I1]=max(fitY1);
+%                     concavity=diff(fitY1,2);
+%                     [B,I]=sort(concavity);
+%                     fitPeaksArray=[fitPeaksArray; fitX1(I(1)),fitY1(I(1))];
+%                     %fitPeaksArray=[fitPeaksArray; fitX1(I1),fitY1(I1)];
+%                     plot(fitPeaksArray(l,1),fitPeaksArray(l,2),'k*','HandleVisibility','off');
+%                 end
+%                 fitPeaksMat(i,j)={fitPeaksArray};
+%             end
+%             %P,ftunnel,windspeed,diameter,sheddingfreq,apeak,fpeak
+%             graphData=[graphData;[repmat(pval,size(fitPeaksMat{i,j},1),1),...
+%             repmat(fTunnel,size(fitPeaksMat{i,j},1),1),repmat(windSpeed,size(fitPeaksMat{i,j},1),1),...
+%             repmat(diameter,size(fitPeaksMat{i,j},1),1),repmat(sheddingFreq,size(fitPeaksMat{i,j},1),1),...
+%             fitPeaksMat{i,j}(:,2),fitPeaksMat{i,j}(:,1)]];
         end
     end
     controllerNames = controllerNames(~cellfun('isempty',controllerNames));     %remove empty strings from controller name list
